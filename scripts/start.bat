8c6fcc88185f1ef1f0f7d4d156655aef41123137
Kuro <148952942+Hapis01@users.noreply.github.com>
2025-07-31 19:24:28 +0700

    Update start.bat

--- scripts/start.bat
+++ scripts/start.bat
@@ -1,3 +1,17 @@
 @echo off
-echo Menjalankan loop.bat...
-start loop.bat
+echo ===========================
+echo = Starting Windows 11... =
+echo ===========================
+
+:: Ekstrak Ngrok ke folder ngrok/
+powershell -Command "Expand-Archive -Path scripts/ngrok.zip -DestinationPath scripts/ngrok"
+
+:: Jalankan ngrok
+cd scripts/ngrok
+start ngrok.exe http 8080
+
+:: Balik ke root
+cd ../..
+
+:: Jalankan sesi lainnya
+call scripts/loop.bat
